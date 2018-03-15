7d115569ea46b658b1ece19b5a2675f5a358f3c4
Vinicius Vaz <viniciushenriquevaz@hotmail.com>
2018-03-15 20:55:29 -0300

    Added Vue and Vuex

---
+++ resources/assets/js/components/app.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>OI</h1>
+    </div>
+</template>
+<script>
+    export default {
+    }
+</script>
+<style lang="scss"></style>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+import Vue from 'vue'
+
+import App from './components/app.vue'
+import growl from 'growl-alert'
+import 'growl-alert/dist/growl-alert.css'
+
+growl.defaults.fadeAway = true
+growl.defaults.fadeAwayTimeout = 3000
+growl.defaults.closeOnClick = true
+
+new Vue({
+    ...App
+}).$mount('#app')
