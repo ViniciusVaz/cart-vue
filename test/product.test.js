7f14ea31660ffb5b9f9863c530cd1c614baf6eb5
Vinicius Vaz <viniciushenriquevaz@hotmail.com>
2018-03-18 20:20:59 -0300

    - Added Jasmine and Karma - Testing component product-item

---
+++ test/product.test.js
@@ -0,0 +1,23 @@
+import { mount } from 'avoriaz'
+import product from '_components/product/product-item.vue'
+
+describe('product.vue', () => {
+	const component = mount(product)
+
+	it('check vue component', () => {
+		expect(component.isVueComponent).toBe(true)
+	})
+
+	it('check essentials visual components', () => {
+		expect(component.contains('.product-item__info__name')).toBe(true)
+		expect(component.contains('.price__value')).toBe(true)
+		expect(component.contains('.product-item__button')).toBe(true)
+	})
+
+	it('check product functions', () => {
+		expect(typeof component.vm.addProduct).toBe('function')
+	})
+
+	
+
+}); // describe('[Org]', () => {...});
