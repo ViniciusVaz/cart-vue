c0549fb9fe0d88e664c4684a1362961d660282a2
Vinicius Vaz <viniciushenriquevaz@hotmail.com>
2018-03-19 22:48:06 -0300

    test heroku

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = (application) => {
     application.get('/products', (req, res) => {
+        console.log(application.app)
         application.app.controllers.index.getProducts( application, req, res )
     })
 }
